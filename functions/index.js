64c51f0a229b07056af699668aaf2a52d2777059
Saran Suthakavatin <>
2020-02-12 12:43:42 +0700

    add files

---
+++ functions/index.js
@@ -0,0 +1,51 @@
+var functions = require('firebase-functions');
+var admin = require('firebase-admin');
+var cors = require('cors')({origin: true});
+var webpush = require('web-push');
+
+
+var serviceAccount = require("./keys.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://web-pwa-fc189.firebaseio.com/'
+});
+
+exports.storePostData = functions.https.onRequest(function (request, response) {
+  cors(request, response, function () {
+    admin.database().ref('posts').push({
+      id: request.body.id,
+      title: request.body.title,
+      location: request.body.location,
+      image: request.body.image
+    })
+      .then(function () {
+        webpush.setVapidDetails('mailto:firebase-adminsdk-huypr@web-pwa-fc189.iam.gserviceaccount.com', 'BFX_pFiY8IgYB1i87CO73ktDTfe1p4EdE-_T3C4_N0xOZCZbPNrBiOXAHnZEFRGC6rnRTOD8pJvK-62ENxhUM2w', '4bk8sdB3ZXjgcKSN1YvfhGyKU2_IjIY7jF-CBmpJmcY');
+        return admin.database().ref('subscriptions').once('value');
+      })
+      .then(function (subscriptions) {
+        subscriptions.forEach(function (sub) {
+          var pushConfig = {
+            endpoint: sub.val().endpoint,
+            keys: {
+              auth: sub.val().keys.auth,
+              p256dh: sub.val().keys.p256dh
+            }
+          };
+
+          webpush.sendNotification(pushConfig, JSON.stringify({
+            title: 'New Post',
+            content: 'New Post added!',
+            openUrl: '/help'
+          }))
+            .catch(function (err) {
+              console.log(err);
+            })
+        });
+        response.status(201).json({message: 'Data stored', id: request.body.id});
+      })
+      .catch(function (err) {
+        response.status(500).json({error: err});
+      });
+  });
+});
